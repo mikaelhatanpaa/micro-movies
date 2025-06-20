8ebd4a2d7e992eb86dee99ece877db5c9b8e933b
Mikael <mikael.hatanpaa@gmail.com>
2025-06-20 22:37:29 +0200

    implemented repository for metadata service
    implemented controller for metadata service
    
    implemented handler for metadata service

---
+++ metadata/cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"movieexample.com/metadata/internal/controller/metadata"
+	httpHandler "movieexample.com/metadata/internal/handler/http"
+	"movieexample.com/metadata/internal/repository/memory"
+)
+
+func main() {
+	log.Println("Starting the movie metadata service")
+
+	repo := memory.New()
+	ctrl := metadata.New(repo)
+	h := httpHandler.New(ctrl)
+
+	http.Handle("/metadata", http.HandlerFunc(h.GetMetadata)) // can use: http.HandleFunc("/metadata", h.GetMetadata)
+
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		panic(err)
+	}
+
+}
